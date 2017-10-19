05646d48cefdedb457269c7403a0207e50f7d0ac
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-10-19 22:31:48 +0000

    Refactor directory structure.

--- iOS/pocketpiglet/main.cpp
+++ iOS/pocketpiglet/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    engine.load(QUrl(QStringLiteral("qrc:/qml/pocketpiglet/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     if (engine.rootObjects().isEmpty())
         return -1;
