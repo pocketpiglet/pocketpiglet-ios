cca5fb7238f855160e5fce69b054a49315f75bb4
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-10-31 19:30:47 +0000

    Updated translations.

--- iOS/pocketpiglet/translations/pocketpiglet_de.ts
+++ iOS/pocketpiglet/translations/pocketpiglet_de.ts
@@ -5,60 +5,60 @@
     <name>PigletFeedPage</name>
     <message>
         <source>Level %1 of %2. Get ready to remember a sandwich recipe...</source>
-        <translation type="unfinished"></translation>
+        <translation>Niveau %1 von %2. Merken Sie sich ein Rezept...</translation>
     </message>
     <message>
         <source>Congratulations, you have just won the game! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ich gratuliere, Sie haben eben gewonnen! Möchten Sie nochmals probieren?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Spiel ist aus. Möchten Sie nochmals probieren?</translation>
     </message>
 </context>
 <context>
     <name>PigletSearchPage</name>
     <message>
         <source>Your piglet wants to play hide-and-seek! Try to find him in your room using your phone&apos;s camera as fast as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dein Ferkel will Verstecken spielen! Versuchen Sie ihn so schnell wie möglich mit der Kamera Ihres Telefons in Ihrem Zimmer zu finden.</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ich gratuliere, du hast einen neuen Highscore! Möchten Sie nochmals probieren?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Spiel ist aus. Möchten Sie nochmals probieren?</translation>
     </message>
 </context>
 <context>
     <name>PigletWashPage</name>
     <message>
         <source>Your piglet wants to take a bath and play with soap bubbles! Help him to catch and pop as many bubbles as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dein Ferkel will ein Bad nehmen und mit Seifenblasen spielen! Hilf ihm, so viele Blasen wie möglich zu fangen und zu knallen.</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ich gratuliere, du hast einen neuen Highscore! Möchten Sie nochmals probieren?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Spiel ist aus. Möchten Sie nochmals probieren?</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>Watch video ad</source>
-        <translation type="unfinished"></translation>
+        <translation>Videoanzeige ansehen</translation>
     </message>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Vollversion kaufen</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Käufe wiederherstellen</translation>
     </message>
 </context>
 </TS>
--- iOS/pocketpiglet/translations/pocketpiglet_fr.ts
+++ iOS/pocketpiglet/translations/pocketpiglet_fr.ts
@@ -5,60 +5,60 @@
     <name>PigletFeedPage</name>
     <message>
         <source>Level %1 of %2. Get ready to remember a sandwich recipe...</source>
-        <translation type="unfinished"></translation>
+        <translation>Niveau %1 de %2. Préparez-vous à mémoriser la recette...</translation>
     </message>
     <message>
         <source>Congratulations, you have just won the game! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Félicitations, vous venez de gagner! Voulez-vous essayer encore une fois?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le jeu est terminé. Voulez-vous essayer encore une fois?</translation>
     </message>
 </context>
 <context>
     <name>PigletSearchPage</name>
     <message>
         <source>Your piglet wants to play hide-and-seek! Try to find him in your room using your phone&apos;s camera as fast as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre porcelet veut jouer à cache-cache! Essayez de le trouver dans votre chambre en utilisant l&apos;appareil photo de votre téléphone aussi vite que possible.</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Félicitations, vous avez un nouveau meilleur score! Voulez-vous jouer à nouveau?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le jeu est terminé. Voulez-vous essayer encore une fois?</translation>
     </message>
 </context>
 <context>
     <name>PigletWashPage</name>
     <message>
         <source>Your piglet wants to take a bath and play with soap bubbles! Help him to catch and pop as many bubbles as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre porcelet veut prendre un bain et jouer avec des bulles de savon! Aidez-le à attraper et à faire éclater autant de bulles que vous le pouvez.</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Félicitations, vous avez un nouveau meilleur score! Voulez-vous jouer à nouveau?</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le jeu est terminé. Voulez-vous essayer encore une fois?</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>Watch video ad</source>
-        <translation type="unfinished"></translation>
+        <translation>Regarder une annonce vidéo</translation>
     </message>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Acheter la version complète</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Restaurer les achats</translation>
     </message>
 </context>
 </TS>
