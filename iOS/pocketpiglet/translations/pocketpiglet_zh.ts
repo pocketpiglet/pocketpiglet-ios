a3441f06929104f12d5ef71e9d54c6bdc0f99fcd
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-12-02 18:38:16 +0000

    Updated translations.
    Former-commit-id: d4d615bfa52307b825fccb3588284af8bcac463a

--- iOS/pocketpiglet/translations/pocketpiglet_zh.ts
+++ iOS/pocketpiglet/translations/pocketpiglet_zh.ts
@@ -5,60 +5,60 @@
     <name>PigletFeedPage</name>
     <message>
         <source>Level %1 of %2. Get ready to remember a sandwich recipe...</source>
-        <translation type="unfinished"></translation>
+        <translation>现在的水平：%1。总计：%2。 准备记住三明治食谱...</translation>
     </message>
     <message>
         <source>Congratulations, you have just won the game! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>恭喜，你刚刚赢得了比赛！ 你想再玩一次吗？</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏结束。 你想再玩一次吗？</translation>
     </message>
 </context>
 <context>
     <name>PigletSearchPage</name>
     <message>
         <source>Your piglet wants to play hide-and-seek! Try to find him in your room using your phone&apos;s camera as fast as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>你的小猪想玩捉迷藏！ 尽可能快地用手机的相机在房间里找到他。</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>恭喜，你有一个新的高分！ 你想再玩一次吗？</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏结束。 你想再玩一次吗？</translation>
     </message>
 </context>
 <context>
     <name>PigletWashPage</name>
     <message>
         <source>Your piglet wants to take a bath and play with soap bubbles! Help him to catch and pop as many bubbles as you can.</source>
-        <translation type="unfinished"></translation>
+        <translation>你的仔猪想要洗澡，玩肥皂泡！ 帮助他抓住并弹出尽可能多的泡泡。</translation>
     </message>
     <message>
         <source>Congratulations, you have a new highscore! Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>恭喜，你有一个新的高分！ 你想再玩一次吗？</translation>
     </message>
     <message>
         <source>Game over. Do you want to play again?</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏结束。 你想再玩一次吗？</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>Watch video ad</source>
-        <translation type="unfinished"></translation>
+        <translation>观看视频广告</translation>
     </message>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>购买完整版本</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>恢复购买</translation>
     </message>
 </context>
 </TS>
