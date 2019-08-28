367f09a268ed9d5ae6084ad35b92d23cca2eacdf
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-29 01:17:49 +0300

    Style nits: always use curly braces even with single statements.

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,9 @@ int main(int argc, char *argv[])
 
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
-    if (engine.rootObjects().isEmpty())
+    if (engine.rootObjects().isEmpty()) {
         return -1;
-
-    return QGuiApplication::exec();
+    } else {
+        return QGuiApplication::exec();
+    }
 }
