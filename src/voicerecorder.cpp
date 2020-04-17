40bff1340302c0d70592e129e6d5013714bbffa2
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-04-17 17:20:25 +0300

    Fix path to directory with Qt patches.

--- src/voicerecorder.cpp
+++ src/voicerecorder.cpp
@@ -279,7 +279,7 @@ void VoiceRecorder::DeleteAudioInput()
 
     //
     // Workaround for Qt for IOS bug with low playback volume when
-    // recording is enabled. See qt-patches contents for details.
+    // recording is enabled. See contents of patches/qt for details.
     //
     QAudioDeviceInfo info = QAudioDeviceInfo::defaultOutputDevice();
 
